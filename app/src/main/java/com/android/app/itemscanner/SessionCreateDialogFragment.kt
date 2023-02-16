121d3c4703925c53c2a752fb6478d7b4af3f1d17
Vanessa Gregoriou <vanessa.e.gregoriou@gmail.com>
2023-02-16 21:47:07 +1100

    Add dialog for starting scan session.

--- app/src/main/java/com/android/app/itemscanner/SessionCreateDialogFragment.kt
+++ app/src/main/java/com/android/app/itemscanner/SessionCreateDialogFragment.kt
@@ -3,7 +3,12 @@ package com.android.app.itemscanner
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import com.android.app.itemscanner.databinding.CreateSessionDialogBinding
 
 class SessionCreateDialogFragment(listener: DialogListener) : DialogFragment() {
 
@@ -22,16 +27,35 @@ class SessionCreateDialogFragment(listener: DialogListener) : DialogFragment() {
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val binding: CreateSessionDialogBinding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.create_session_dialog, null, false
+        )
+
+        binding.numPhotosSlider.value = 180f
+        binding.numPhotosSlider.addOnChangeListener { slider, value, fromUser ->
+            binding.editNumPhotos.setText(
+                value.toInt().toString()
+            )
+        }
+        binding.editNumPhotos.setText("180")
+        binding.editNumPhotos.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                binding.numPhotosSlider.value = s.toString().toFloat()
+            }
+        })
+
+        val dialogView = binding.root
+        dialogView.tag = binding
+
         return activity?.let {
             // Use the Builder class for convenient dialog construction
             val builder = AlertDialog.Builder(it)
-            // Get the layout inflater
-            val inflater = requireActivity().layoutInflater
             builder
-                .setTitle(R.string.add_first_session)
-                .setView(inflater.inflate(R.layout.create_session_dialog, null))
-                .setPositiveButton(R.string.start_scanning) { _, _ ->
-                    listener.onStart(this)
+                .setView(dialogView)
+                .setPositiveButton(R.string.start_scanning) { dialog, _ ->
+
                 }
                 .setNegativeButton(R.string.close) { _, _ ->
                     listener.onClose(this)
